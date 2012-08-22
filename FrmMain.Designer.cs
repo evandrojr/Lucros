fa4111904fe0871ae97203412e33edcf92ee3477
evandrojr <evandrojr@gmail.com>
2012-08-22 16:01:38 -0300

    label included

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -39,6 +39,7 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // dtpStartDate
@@ -98,7 +99,7 @@
             // 
             // txtInterest
             // 
-            this.txtInterest.Location = new System.Drawing.Point(461, 133);
+            this.txtInterest.Location = new System.Drawing.Point(461, 151);
             this.txtInterest.Name = "txtInterest";
             this.txtInterest.Size = new System.Drawing.Size(185, 20);
             this.txtInterest.TabIndex = 7;
@@ -111,7 +112,7 @@
             "Diário",
             "Mensal",
             "Anual"});
-            this.lbRendimento.Location = new System.Drawing.Point(160, 170);
+            this.lbRendimento.Location = new System.Drawing.Point(102, 151);
             this.lbRendimento.Name = "lbRendimento";
             this.lbRendimento.Size = new System.Drawing.Size(50, 43);
             this.lbRendimento.TabIndex = 8;
@@ -171,11 +172,21 @@
             this.label5.TabIndex = 14;
             this.label5.Text = "Frequencia do rendimento (ainda não está funcionando)";
             // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(458, 132);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(141, 13);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Rendimento do investimento";
+            // 
             // FrmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(661, 225);
+            this.ClientSize = new System.Drawing.Size(654, 223);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -214,6 +225,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
