5af5a170ba3ca8967c590b06e47fcb4240d7bb7a
evandrojr <evandrojr@gmail.com>
2012-08-22 15:59:17 -0300

    1st push

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Lucros
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.dtpStartDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpEndDate = new System.Windows.Forms.DateTimePicker();
+            this.tbNumDays = new System.Windows.Forms.TextBox();
+            this.tbInitialValue = new System.Windows.Forms.TextBox();
+            this.tbFinalValue = new System.Windows.Forms.TextBox();
+            this.txtProfit = new System.Windows.Forms.TextBox();
+            this.txtInterest = new System.Windows.Forms.TextBox();
+            this.lbRendimento = new System.Windows.Forms.ListBox();
+            this.btEvaluate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dtpStartDate
+            // 
+            this.dtpStartDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStartDate.Location = new System.Drawing.Point(10, 43);
+            this.dtpStartDate.Name = "dtpStartDate";
+            this.dtpStartDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpStartDate.TabIndex = 1;
+            this.dtpStartDate.ValueChanged += new System.EventHandler(this.dtpStartDate_ValueChanged);
+            // 
+            // dtpEndDate
+            // 
+            this.dtpEndDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEndDate.Location = new System.Drawing.Point(229, 43);
+            this.dtpEndDate.Name = "dtpEndDate";
+            this.dtpEndDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpEndDate.TabIndex = 2;
+            this.dtpEndDate.ValueChanged += new System.EventHandler(this.dtpEndDate_ValueChanged);
+            // 
+            // tbNumDays
+            // 
+            this.tbNumDays.Location = new System.Drawing.Point(460, 43);
+            this.tbNumDays.Name = "tbNumDays";
+            this.tbNumDays.ReadOnly = true;
+            this.tbNumDays.Size = new System.Drawing.Size(185, 20);
+            this.tbNumDays.TabIndex = 3;
+            this.tbNumDays.Text = "0";
+            this.tbNumDays.Validating += new System.ComponentModel.CancelEventHandler(this.tbNumDays_Validating);
+            // 
+            // tbInitialValue
+            // 
+            this.tbInitialValue.Location = new System.Drawing.Point(10, 90);
+            this.tbInitialValue.Name = "tbInitialValue";
+            this.tbInitialValue.Size = new System.Drawing.Size(200, 20);
+            this.tbInitialValue.TabIndex = 4;
+            this.tbInitialValue.Text = "0";
+            this.tbInitialValue.TextChanged += new System.EventHandler(this.tbInitialValue_TextChanged);
+            // 
+            // tbFinalValue
+            // 
+            this.tbFinalValue.Location = new System.Drawing.Point(229, 90);
+            this.tbFinalValue.Name = "tbFinalValue";
+            this.tbFinalValue.Size = new System.Drawing.Size(200, 20);
+            this.tbFinalValue.TabIndex = 5;
+            this.tbFinalValue.Text = "0";
+            this.tbFinalValue.TextChanged += new System.EventHandler(this.tbFinalValue_TextChanged);
+            // 
+            // txtProfit
+            // 
+            this.txtProfit.Location = new System.Drawing.Point(460, 90);
+            this.txtProfit.Name = "txtProfit";
+            this.txtProfit.ReadOnly = true;
+            this.txtProfit.Size = new System.Drawing.Size(185, 20);
+            this.txtProfit.TabIndex = 6;
+            this.txtProfit.Text = "0";
+            // 
+            // txtInterest
+            // 
+            this.txtInterest.Location = new System.Drawing.Point(461, 133);
+            this.txtInterest.Name = "txtInterest";
+            this.txtInterest.Size = new System.Drawing.Size(185, 20);
+            this.txtInterest.TabIndex = 7;
+            this.txtInterest.Text = "0";
+            // 
+            // lbRendimento
+            // 
+            this.lbRendimento.FormattingEnabled = true;
+            this.lbRendimento.Items.AddRange(new object[] {
+            "Diário",
+            "Mensal",
+            "Anual"});
+            this.lbRendimento.Location = new System.Drawing.Point(160, 170);
+            this.lbRendimento.Name = "lbRendimento";
+            this.lbRendimento.Size = new System.Drawing.Size(50, 43);
+            this.lbRendimento.TabIndex = 8;
+            // 
+            // btEvaluate
+            // 
+            this.btEvaluate.Location = new System.Drawing.Point(460, 179);
+            this.btEvaluate.Name = "btEvaluate";
+            this.btEvaluate.Size = new System.Drawing.Size(185, 34);
+            this.btEvaluate.TabIndex = 9;
+            this.btEvaluate.Text = "Calcular";
+            this.btEvaluate.UseVisualStyleBackColor = true;
+            this.btEvaluate.Click += new System.EventHandler(this.btEvaluate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Início da aplicação";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Fim da aplicação";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Valor aplicado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 74);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(98, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Valor líquido obtido";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(271, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Frequencia do rendimento (ainda não está funcionando)";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 225);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btEvaluate);
+            this.Controls.Add(this.lbRendimento);
+            this.Controls.Add(this.txtInterest);
+            this.Controls.Add(this.txtProfit);
+            this.Controls.Add(this.tbFinalValue);
+            this.Controls.Add(this.tbInitialValue);
+            this.Controls.Add(this.tbNumDays);
+            this.Controls.Add(this.dtpEndDate);
+            this.Controls.Add(this.dtpStartDate);
+            this.Name = "FrmMain";
+            this.Text = "Lucros";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dtpStartDate;
+        private System.Windows.Forms.DateTimePicker dtpEndDate;
+        private System.Windows.Forms.TextBox tbNumDays;
+        private System.Windows.Forms.TextBox tbInitialValue;
+        private System.Windows.Forms.TextBox tbFinalValue;
+        private System.Windows.Forms.TextBox txtProfit;
+        private System.Windows.Forms.TextBox txtInterest;
+        private System.Windows.Forms.ListBox lbRendimento;
+        private System.Windows.Forms.Button btEvaluate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Lucros
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain() {
+            InitializeComponent();
+        }
+
+//        decimal moneyvalue = 1921.39m;
+//string html = String.Format("Order Total: {0:C}", moneyvalue);
+//Console.WriteLine(html);
+
+        private void FrmMain_Load(object sender, EventArgs e) {
+            lbRendimento.SelectedIndex = 1;
+        }
+
+        private void tbNumDays_Validating(object sender, CancelEventArgs e) {
+    
+            decimal value;
+            TextBox tb = (TextBox)sender;
+
+            if (tb.Text == "") {
+                tb.Text = "0";
+                return;
+            }
+
+            if (decimal.TryParse(tb.Text,NumberStyles.Currency, null, out value)) {
+            // Do something with the value
+            } else {
+                MessageBox.Show("Formato inválido");
+                e.Cancel = true;
+            }
+    
+        }
+
+        private void calculateNumDays(){
+
+            TimeSpan ts = new TimeSpan();
+            ts = (dtpEndDate.Value - dtpStartDate.Value);
+            tbNumDays.Text = ts.Days.ToString();
+        }
+
+        private void calculateProfit() {
+            try {
+                txtProfit.Text = Convert.ToString(Convert.ToDouble(tbFinalValue.Text) - Convert.ToDouble(tbInitialValue.Text));
+            } catch { };
+        }
+
+        private void dtpStartDate_ValueChanged(object sender, EventArgs e) {
+            calculateNumDays();
+        }
+
+        private void dtpEndDate_ValueChanged(object sender, EventArgs e) {
+            calculateNumDays();
+        }
+
+        private void tbFinalValue_TextChanged(object sender, EventArgs e) {
+            calculateProfit();
+        }
+
+        private void tbInitialValue_TextChanged(object sender, EventArgs e) {
+            calculateProfit();
+        }
+
+        private void btEvaluate_Click(object sender, EventArgs e) {
+            double interest, vi, vf, n;
+
+            vi = Convert.ToDouble(tbInitialValue.Text);
+            vf = Convert.ToDouble(tbFinalValue.Text);
+            n = Convert.ToDouble(tbNumDays.Text) / 30;
+            interest = (Math.Pow(vf / vi, 1 / n) - 1) * 100;
+            txtInterest.Text = interest.ToString();
+        }
+
+
+    }
+}
